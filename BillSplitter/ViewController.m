bc26edfc1e6c984fd2fdc8525e01f3d9633ebc61
Nicholas Fung <nbfung@sfu.ca>
2017-10-16 00:57:18 -0700

    Implement calculation of the split bill

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billTotalTextField;
+@property (weak, nonatomic) IBOutlet UISlider *totalPersonsSlider;
+@property (weak, nonatomic) IBOutlet UILabel *billPerPersonLabel;
 
 @end
 
@@ -18,6 +21,45 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+- (IBAction)splitBillAction:(id)sender {
+    if (![self.billTotalTextField.text doubleValue]) {
+        return;
+    }
+    NSNumberFormatter *moneyFormetter = [[NSNumberFormatter alloc] init];
+    [moneyFormetter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    NSDecimalNumber *billAmount = [[NSDecimalNumber alloc] initWithString: self.billTotalTextField.text];
+    NSDecimalNumber *totalPeople = [[NSDecimalNumber alloc] initWithFloat:self.totalPersonsSlider.value];
+    NSDecimalNumber *amountPerPerson = [billAmount decimalNumberByDividingBy:totalPeople];
+    self.billPerPersonLabel.text = [NSString stringWithFormat:@"Each of the %d people pay %@", (int)self.totalPersonsSlider.value, [moneyFormetter stringFromNumber:amountPerPerson]];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    [self splitBillAction:self];
+}
+
+
+- (IBAction)totalPersonsSliderValueChanged:(UISlider *)sender {
+    [sender setValue: roundf(sender.value)];
+    NSLog(@"Slider value is %f", sender.value);
+    [self splitBillAction:self];
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    NSCharacterSet *decimalNumberCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@"1234567890."];
+    NSRange testForNonNumbers = [string rangeOfCharacterFromSet:[decimalNumberCharacterSet invertedSet]];
+    if (testForNonNumbers.length == 0) {
+        if ([textField.text containsString:@"."] && [string containsString:@"."]) { //text field already has a decimal point
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+    [self splitBillAction:self];
+    return true;
+}
+
 
 
 - (void)didReceiveMemoryWarning {
